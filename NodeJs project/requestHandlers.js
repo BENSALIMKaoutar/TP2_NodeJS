88eeeb73934983ea4d067c8f67a07a11bf982412
BENSALIMKaoutar <118397882+BENSALIMKaoutar@users.noreply.github.com>
2024-05-14 17:30:08 +0100

    Add files via upload

--- NodeJs project/requestHandlers.js
+++ NodeJs project/requestHandlers.js
@@ -1,11 +1,23 @@
 var exec = require("child_process").exec;
+var fs = require ('fs');
 function start(response) {
-console.log("Request handler 'start' was called.");
-setTimeout( function() {
-response.writeHead(200, {"Content-Type": "text/plain"});
-response.write("Hello start");
-response.end();}, 60000);
+    console.log("Request handler 'start' was called.");
+    fs.readFile('start.html', function(err, htmlContent) {
+        if (err) {
+            // En cas d'erreur de lecture du fichier
+            response.writeHead(500, { "Content-Type": "text/plain" });
+            response.write("Erreur interne du serveur.");
+            response.end();
+        } else {
+            // Si la lecture du fichier est réussie, renvoyer le contenu HTML
+            response.writeHead(200, { "Content-Type": "text/html" });
+            response.write(htmlContent);
+            response.end();
+        }
+    });
 }
+
+
 function find(response) {
 console.log("Request handler 'start' was called.");
 exec("find /",
@@ -18,26 +30,69 @@ response.end();
 }
 function upload(response) {
 console.log("Request handler 'upload' was called.");
-response.writeHead(200, {"Content-Type": "text/plain"});
-response.write("Hello Upload");
-response.end();
+fs.readFile('upload.html', function(err,htmlContent){
+        if (err) {
+            // En cas d'erreur de lecture du fichier
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write("Erreur interne du serveur.");
+            response.end();
+        } else {
+            // Si la lecture du fichier est réussie, renvoyer le contenu HTML
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(htmlContent);
+            response.end();
+        }
+    });
 }
 function show(response) {
 console.log("Request handler 'show' was called.");
-response.writeHead(200, {"Content-Type": "text/plain"});
-response.write("Hello Show");
-response.end();
+fs.readFile('show.html', function(err,htmlContent){
+        if (err) {
+            // En cas d'erreur de lecture du fichier
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write("Erreur interne du serveur.");
+            response.end();
+        } else {
+            // Si la lecture du fichier est réussie, renvoyer le contenu HTML
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(htmlContent);
+            response.end();
+        }
+    });
 }
-function login(response ) {
+function login(response) {
 console.log("Request handler 'login' was called.");
-response.writeHead(200, {"Content-Type": "text/plain"});
-response.write("Hello Login");
-response.end();
+fs.readFile('login.html', function(err,htmlContent){
+	if (err) {
+            // En cas d'erreur de lecture du fichier
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write("Erreur interne du serveur.");
+            response.end();
+        } else {
+            // Si la lecture du fichier est réussie, renvoyer le contenu HTML
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(htmlContent);
+            response.end();
+        }
+    });
 }
 function logout(response) {
-console.log("Request handler 'logout' was called.");
-return "Hello Logout"
+console.log("Request handler 'start' was called.");
+fs.readFile('logout.html', function(err,htmlContent){
+        if (err) {
+            // En cas d'erreur de lecture du fichier
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write("Erreur interne du serveur.");
+            response.end();
+        } else {
+            // Si la lecture du fichier est réussie, renvoyer le contenu HTML
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(htmlContent);
+            response.end();
+        }
+    });
 }
+
 exports.start = start;
 exports.upload = upload;
 exports.find = find;
